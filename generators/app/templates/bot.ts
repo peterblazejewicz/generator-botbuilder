ddbb64227ca20876ce4755fbdfe57dff5f7b4943
Christopher Harrison <chrhar@microsoft.com>
2017-02-03 17:52:21 -0800

    Updated for 3.5

--- generators/app/templates/bot.ts
+++ generators/app/templates/bot.ts
@@ -1,20 +1,22 @@
 import * as builder from 'botbuilder';
 
-const bot = new builder.UniversalBot(
-    new builder.ChatConnector(), [
-        (session, args, next) => {
-            const botName = '<%= botName %>';
-            const description = `<%= description %>`;
+export default class extends builder.UniversalBot {
+    constructor() {
+        super(new builder.ChatConnector(),
+            [
+                (session, args, next) => {
+                    const botName = 'sample';
+                    const description = `sample`;
 
-            session.send(`Hi there! I'm ${botName}`);
-            session.send(`In a nutshell, here's what I can do:\n\n${description}`);
+                    session.send(`Hi there! I'm ${botName}`);
+                    session.send(`In a nutshell, here's what I can do:\n\n${description}`);
 
-            builder.Prompts.text(session, `What's your name?`);
-        },
-        (session, results, next) => {
-            session.endConversation(`Welcome, ${results.response}`);
-        },
-    ]
-)
-
-export default bot;
+                    builder.Prompts.text(session, `What's your name?`);
+                },
+                (session, results, next) => {
+                    session.endConversation(`Welcome, ${results.response}`);
+                },
+            ]
+        );
+    }
+}
